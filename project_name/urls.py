3216003b5d6af1911fdebf2471efa8b8bb6fa10b
Marc Puig <mpuig@qibbus.com>
2012-10-30 17:02:58 +0100

    Base template and Bootstrap css/js framework added

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
 
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf
@@ -8,6 +9,7 @@ admin.autodiscover()
 
 # See: https://docs.djangoproject.com/en/dev/topics/http/urls/
 urlpatterns = patterns('',
+    url(r'^$', direct_to_template, {'template': 'home.html'}, name='home'),
     # Admin panel and documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
